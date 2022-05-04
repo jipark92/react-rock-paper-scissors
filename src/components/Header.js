73b0cfe2226a3802d3aa3a1f9d7cc9e78efa2f8e
ji <jiyongprk@gmail.com>
2022-05-04 17:08:24 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className='title-container'>
+            <h1>React Rock Paper Scissors</h1>
+        </header>
+    )
+}
---
+++ src/components/game.js
@@ -0,0 +1,5 @@
+export default function game() {
+    return (
+        <div>game</div>
+    )
+}
